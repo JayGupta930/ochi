8d1f88044fd54a0ae0ebe8af0abe09ddf0f37425
Jaygupta930 <jayguptajaygupta8@gmail.com>
2025-05-04 22:14:53 +0530

    feat: add Marquee component with animated text display

---
+++ src/components/Marquee.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { motion } from "motion/react";
+
+function Marquee() {
+  return (
+    <div className="w-full py-20 rounded-tl-3xl rounded-tr-3xl bg-[#004d43]">
+      <div className="text border-t-2 border-b-2 border-zinc-300 gap-10 overflow-hidden flex whitespace-nowrap">
+        <motion.h1 initial={{x: 0}} animate={{x: "-100%"}} transition={{ease: "linear", repeat: Infinity, duration: 5}} className='text-[14vw] leading-none font-["Founders Grotesk"] font-semibold uppercase mb-8 pt-8'>
+          {" "}
+          We are ochi{" "}
+        </motion.h1>
+        <motion.h1 initial={{x: 0}} animate={{x: "-100%"}} transition={{ease: "linear", repeat: Infinity, duration: 5}} className='text-[14vw] leading-none font-["Founders Grotesk"] font-semibold uppercase mb-8 pt-8'>
+          {" "}
+          We are ochi
+        </motion.h1>
+        <motion.h1 initial={{x: 0}} animate={{x: "-100%"}} transition={{ease: "linear", repeat: Infinity, duration: 5}} className='text-[14vw] leading-none font-["Founders Grotesk"] font-semibold uppercase mb-8 pt-8'>
+          {" "}
+          We are ochi
+        </motion.h1>
+      </div>
+    </div>
+  );
+}
+
+export default Marquee;
