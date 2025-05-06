897ec935b4c63164c0a76e5adb96973edc3f6fb0
Jaygupta930 <jayguptajaygupta8@gmail.com>
2025-05-06 09:59:28 +0530

    feat: add scroll properties to Marquee component for improved animation effects

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -3,7 +3,7 @@ import { motion } from "motion/react";
 
 function Marquee() {
   return (
-    <div className="w-full py-20 rounded-tl-3xl rounded-tr-3xl bg-[#004d43]">
+    <div data-scroll data-scroll-section data-scroll-speed = '.2' className="w-full py-20 rounded-tl-3xl rounded-tr-3xl bg-[#004d43]">
       <div className="text border-t-2 border-b-2 border-zinc-300 overflow-hidden flex whitespace-nowrap">
         <motion.h1
           initial={{ x: 0 }}
